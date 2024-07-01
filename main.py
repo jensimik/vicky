e0099e6f7cf18be52fa823401e1eb52ad69bd748
Jens Davidsen <jens@gnerd.dk>
2024-07-01 23:26:22 +0200

    victron device objects

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import machine
 import time
 import vga1_16x32 as font
 from st7789py import ST7789, WHITE, BLACK
-from victron_ble import VictronBLE
+from victron_ble import VictronBLE, VictronSolar, VictronDCDC, VictronMonitor
 
 power = machine.Pin(4, machine.Pin.OUT)  # when not powered via USB
 power.on()
@@ -25,30 +25,33 @@ lcd = ST7789(
 lcd.fill(BLACK)
 
 
-CONFIG = {
-    b"\xee\xc0\xb8\x8c\x53\xf4": {
-        "type": "SOLAR",
-        "offset": 8,
-        "key": b"\x10\x63\x76\x13\x6f\xf4\xd0\x8c\x6a\x01\x99\x15\xfd\xee\xc0\x11",
-    },
-    b"\xee\xc0\xb8\x8c\x53\xf3": {
-        "type": "DCDC",
-        "offset": 52,
-        "key": b"\x10\x63\x76\x13\x6f\xf4\xd0\x8c\x6a\x01\x99\x15\xfd\xee\xc0\x11",
-    },
-    b"\xee\xc0\xb8\x8c\x53\xf2": {
-        "type": "MON",
-        "offset": 96,
-        "key": b"\x10\x63\x76\x13\x6f\xf4\xd0\x8c\x6a\x01\x99\x15\xfd\xee\xc0\x11",
-    },
-}
-
-
 def display_func(text, offset):
     lcd.text(font, text, 6, offset, WHITE, BLACK)
 
 
-victron = VictronBLE(config=CONFIG, display_func=display_func)
+solar = VictronSolar(
+    mac=b"\xee\xc0\xb8\x8c\x53\xf4",
+    key=b"\x10\x63\x76\x13\x6f\xf4\xd0\x8c\x6a\x01\x99\x15\xfd\xee\xc0\x11",
+    text_format="{mode:<7} {solar_power:>5.0f}W",
+    callback=lambda text: display_func(text, 8),
+)
+dcdc = VictronDCDC(
+    mac=b"\xee\xc0\xb8\x8c\x53\xf3",
+    key=b"\x10\x63\x76\x13\x6f\xf4\xd0\x8c\x6a\x01\x99\x15\xfd\xee\xc0\x11",
+    text_format="{mode:<7}",
+    callback=lambda text: display_func(text, 52),
+)
+monitor = VictronMonitor(
+    mac=b"\xee\xc0\xb8\x8c\x53\xf2",
+    key=b"\x10\x63\x76\x13\x6f\xf4\xd0\x8c\x6a\x01\x99\x15\xfd\xee\xc0\x11",
+    text_format="{remaining_mins:<3}M {current:>3+.0f}A {soc:>3}%",
+    callback=lambda text: display_func(text, 96),
+)
+
+victron = VictronBLE()
+victron.register_device(solar)
+victron.register_device(dcdc)
+victron.register_device(monitor)
 victron.start()
 
 
--- victron_ble.py
+++ victron_ble.py
@@ -1,6 +1,4 @@
-import machine
 import struct
-import time
 from micropython import const
 from bluetooth import BLE
 from cryptolib import aes
@@ -17,11 +15,108 @@ MODES = {
 }
 
 
+class DeviceMeta(type):
+    """A Device metaclass that will be used for device class creation."""
+
+    def __instancecheck__(cls, instance):
+        return cls.__subclasscheck__(type(instance))
+
+    def __subclasscheck__(cls, subclass):
+        return hasattr(subclass, "parse") and callable(subclass.parse)
+
+
+class VictronDevice(metaclass=Devicemeta):
+    def __init__(self, mac, key, text_format, callback):
+        self._mac = mac
+        self._key = key
+        self._text_format = text_format
+        self.callback = callback
+        self._offset = offset
+
+    def uncipher(self, adv_data) -> str:
+        ctr = bytearray(adv_data[12:14])
+        ctr.extend(bytes(14))
+        ciphertext = bytearray(adv_data[15:])
+        if len(adv_data[15:]) < 16:
+            ciphertext.extend(bytes(16 - len(adv_data[15:])))
+        cipher = aes(self._key, 1)
+        cipher.encrypt(ctr, ctr)
+        cleartext = bytes(a ^ b for a, b in zip(ciphertext, ctr))
+        return cleartext
+
+
+class VictronSolar(VictronDevice):
+    def parse(self, cleartext) -> dict:
+        (
+            state,
+            error,
+            battery_voltage,
+            battery_charging_current,
+            yield_today,
+            solar_power,
+            external_device_load,
+        ) = struct.unpack("BBhhHHH", cleartext)
+        mode = MODES[state]
+        battery_charging_current = battery_charging_current / 10
+        external_device_load = (
+            0 if external_device_load == 0x1FF else external_device_load
+        )
+        return {
+            "state": state,
+            "error": error,
+            "battery_voltage": battery_voltage,
+            "battery_charging_current": battery_charging_current,
+            "yield_today": yield_today,
+            "solar_power": solar_power,
+            "external_device_load": external_device_load,
+        }
+
+
+class VictronDCDC(VictronDevice):
+    def parse(self, cleartext) -> dict:
+        state, error, input_voltage, output_voltage, off_reason = struct.unpack(
+            "BBhhI", cleartext
+        )
+        mode = MODES[state]
+        return {
+            "state": state,
+            "error": error,
+            "input_voltage": input_voltage,
+            "output_voltage": output_voltage,
+            "off_reason": off_reason,
+        }
+
+
+class VictronMonitor(VictronDevice):
+    def parse(self, cleartext) -> dict:
+        # cannot parse current in 24bit field skipping those bytes with x
+        remaining_mins, voltage, alarm, aux, consumed_ah, soc = struct.unpack(
+            "HHHHxxxHH", cleartext
+        )
+        voltage = voltage / 100
+        consumed_ah = consumed_ah / 100
+        soc = ((soc & 0x3FFF) >> 4) / 10
+        # get current
+        chunk = bytes(cleartext[4:7])
+        current = (
+            struct.unpack("<i", chunk + ("\0" if chunk[2] < 128 else "\xff")) / 1000
+        )
+        remaining_mins = 999 if remaining_mins > 999 else remaining_mins
+        return {
+            "remaining_mins": remaining_mins,
+            "voltage": voltage,
+            "consumed_ah": consumed_ah,
+            "current": current,
+            "soc": soc,
+        }
+
+
 class VictronBLE:
-    def __init__(self, config={}, display_func=None):
-        self.config = config
-        self._display_func = display_func
-        self._MACS = list(config.keys())
+    def __init__(self):
+        self._MACS = {}
+
+    def register_device(self, device):
+        self._MACS[device._mac] = device
 
     def start(self):
         BLE().active(True)
@@ -42,58 +137,8 @@ class VictronBLE:
                 and adv_data[1:2] == b"\x01"
             ):
                 if addr in self._MACS:
-                    config = self.config[bytes(addr)]
-                    ctr = bytearray(adv_data[12:14])
-                    ctr.extend(bytes(14))
-                    ciphertext = bytearray(adv_data[15:])
-                    if len(adv_data[15:]) < 16:
-                        ciphertext.extend(bytes(16 - len(adv_data[15:])))
-                    cipher = aes(config["key"], 1)
-                    cipher.encrypt(ctr, ctr)
-                    cleartext = bytes(a ^ b for a, b in zip(ciphertext, ctr))
-                    if config["type"] == "SOLAR":
-                        (
-                            state,
-                            error,
-                            battery_voltage,
-                            battery_charging_current,
-                            yield_today,
-                            solar_power,
-                            external_device_load,
-                        ) = struct.unpack("BBhhHHH", cleartext)
-                        mode = MODES[state]
-                        battery_charging_current = battery_charging_current / 10
-                        external_device_load = (
-                            0 if external_device_load == 0x1FF else external_device_load
-                        )
-                        self._display_func(
-                            f"{mode:<7} {solar_power:>5.0f}W",
-                            config["offset"],
-                        )
-                    elif config["type"] == "DCDC":
-                        state, error, input_voltage, output_voltage, off_reason = (
-                            struct.unpack("BBhhI", cleartext)
-                        )
-                        mode = MODES[state]
-                        self._display_func(f"{mode:<7}", config["offset"])
-                    elif config["type"] == "MON":
-                        # cannot parse current in 24bit field skipping those bytes with x
-                        remaining_mins, voltage, alarm, aux, consumed_ah, soc = (
-                            struct.unpack("HHHHxxxHH", cleartext)
-                        )
-                        voltage = voltage / 100
-                        consumed_ah = consumed_ah / 100
-                        soc = ((soc & 0x3FFF) >> 4) / 10
-                        # get current
-                        chunk = bytes(cleartext[4:7])
-                        current = (
-                            struct.unpack(
-                                "<i", chunk + ("\0" if chunk[2] < 128 else "\xff")
-                            )
-                            / 1000
-                        )
-                        remaining_mins = 999 if remaining_mins > 999 else remaining_mins
-                        self._display_func(
-                            f"{remaining_mins:<3}M {current:>3+.0f}A {soc:>3}%",
-                            config["offset"],
-                        )
+                    device = self._MACS[bytes(addr)]
+                    cleartext = device.uncipher(adv_data)
+                    data = device.parse(cleartext)
+                    text = device.text_format.format(**data)
+                    device.callback(text)
